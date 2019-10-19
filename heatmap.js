1b852c9cdefec7b8641a3434fa0b6ba291bd1fc4
paniterka <teresa.kubacka+github@gmail.com>
2019-10-20 00:10:53 +0200

    render the athlete plot

--- heatmap.js
+++ heatmap.js
@@ -1,6 +1,8 @@
-var margin = {top: 80, right: 25, bottom: 30, left: 40},
-  width = 450 - margin.left - margin.right,
-  height = 450 - margin.top - margin.bottom;
+var margin = {top: 80, right: 25, bottom: 30, left: 100},
+vizx = 600,
+vizy = 600,
+  width = vizx - margin.left - margin.right,
+  height = vizy - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -12,37 +14,61 @@ var svg = d3.select("#my_dataviz")
         "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
+// d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
 
+d3.csv("athletes_year_f2.csv", function(data) {
+
+    console.log(data)
   // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-  var myGroups = d3.map(data, function(d){return d.group;}).keys()
-  var myVars = d3.map(data, function(d){return d.variable;}).keys()
+  var myGroups = d3.map(data, function(d){return d.Year;}).keys()
+  var myVars = d3.map(data, function(d){return d.Athlete;}).keys()
+
+  var xy_padding = 0.15; 
 
   // Build X scales and axis:
   var x = d3.scaleBand()
     .range([ 0, width ])
     .domain(myGroups)
-    .padding(0.05);
-  svg.append("g")
-    .style("font-size", 15)
+    .padding(xy_padding);
+
+svg.append("g")
+    .style("font-size", 10)
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(0))
-    .select(".domain").remove()
+    .call(d3.axisBottom(x)
+        .tickSize(0)
+        .tickFormat(function(d){return Math.round(d)})
+        )
+    .call( g => g.select(".domain").remove() )
+    .selectAll("text")	
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-90)")
+
 
   // Build Y scales and axis:
   var y = d3.scaleBand()
     .range([ height, 0 ])
     .domain(myVars)
-    .padding(0.05);
+    .padding(xy_padding);
   svg.append("g")
-    .style("font-size", 15)
+    .style("font-size", 10)
     .call(d3.axisLeft(y).tickSize(0))
     .select(".domain").remove()
 
   // Build color scale
-  var myColor = d3.scaleSequential()
-    .interpolator(d3.interpolateInferno)
-    .domain([1,100])
+
+  var myColor = {
+      "0.0": "ghostwhite", 
+      "1.0": "gold", 
+      "2.0": "silver", 
+      "3.0": "peru"
+  }
+  console.log(myColor[0])
+
+//   var myColor = d3.scaleSequential()
+//     .interpolator(d3.interpolateInferno)
+//     .domain([1,100])
 
   // create a tooltip
   var tooltip = d3.select("#my_dataviz")
@@ -51,8 +77,8 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/he
     .attr("class", "tooltip")
     .style("background-color", "white")
     .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
+    .style("border-width", "1px")
+    .style("border-radius", "2px")
     .style("padding", "5px")
 
   // Three function that change the tooltip when user hover / move / leave a cell
@@ -65,8 +91,8 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/he
   }
   var mousemove = function(d) {
     tooltip
-      .html("The exact value of<br>this cell is: " + d.value)
-      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .html("Finish time: " + d.Time)
+      .style("left", (d3.mouse(this)[0]) + "px")
       .style("top", (d3.mouse(this)[1]) + "px")
   }
   var mouseleave = function(d) {
@@ -79,17 +105,17 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/he
 
   // add the squares
   svg.selectAll()
-    .data(data, function(d) {return d.group+':'+d.variable;})
+    .data(data, function(d) {return d.Year+':'+d.Athlete;})
     .enter()
     .append("rect")
-      .attr("x", function(d) { return x(d.group) })
-      .attr("y", function(d) { return y(d.variable) })
-      .attr("rx", 4)
-      .attr("ry", 4)
+      .attr("x", function(d) { return x(d.Year) })
+      .attr("y", function(d) { return y(d.Athlete) })
+      .attr("rx", 3)
+      .attr("ry", 3)
       .attr("width", x.bandwidth() )
       .attr("height", y.bandwidth() )
-      .style("fill", function(d) { return myColor(d.value)} )
-      .style("stroke-width", 4)
+      .style("fill", function(d) { return myColor[d.Place_no]} )
+      .style("stroke-width", 2)
       .style("stroke", "none")
       .style("opacity", 0.8)
     .on("mouseover", mouseover)
@@ -103,7 +129,7 @@ svg.append("text")
         .attr("y", -50)
         .attr("text-anchor", "left")
         .style("font-size", "22px")
-        .text("A d3.js heatmap");
+        .text("Ironman World Championship");
 
 // Add subtitle to graph
 svg.append("text")
@@ -113,4 +139,4 @@ svg.append("text")
         .style("font-size", "14px")
         .style("fill", "grey")
         .style("max-width", 400)
-        .text("A short description of the take-away message of this chart.");
+        .text("Game of skill - game of chance");
